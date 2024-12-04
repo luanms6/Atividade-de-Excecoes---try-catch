781b292f897cfabdf7f4366f72ad994d5eab4fda
Luan Peixoto <peixotoluan2023@gmail.com>
2024-12-04 16:49:27 -0300

    Aplicando pilha de chamadas stack trace

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,7 +6,20 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args){
+        method1();
 
+        System.out.println("End for program");  
+    }
+
+    public static void method1(){
+        System.out.println("***METHOD1 START***");
+        method2();
+        System.out.println("***METHOD1 END***");
+    }
+
+    public static void method2(){
+
+        System.out.println("***METHOD2 START***");
         Scanner sc = new Scanner(System.in);
 
         try{
@@ -18,14 +31,15 @@ public class Main{
         
         catch(ArrayIndexOutOfBoundsException e){
             System.out.println("Invalid position");
+            e.printStackTrace();
+            sc.next();
         } 
 
         catch(InputMismatchException e){
             System.out.println("Input error");
         }
 
-        System.out.println("End for program");
-
         sc.close();
+        System.out.println("***METHOD2 END***");
     }
 }
